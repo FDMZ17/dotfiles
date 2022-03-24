8dccde856ce41d472ac93d2cb9ab80f5241268be
FDMZ17 <85776604+FDMZ17@users.noreply.github.com>
2022-03-24 19:58:58 +0700

    fix the ss.sh file

--- fdwm/script/ss.sh
+++ fdwm/script/ss.sh
@@ -6,7 +6,7 @@ function ss {
 	if [[ $selected =  "upload" ]]; then
 		captureDate=$(date '+%d-%m-%y-%H:%M:%S')
 		maim --select /tmp/${captureDate}.png
-		curl -F file=@/tmp/${captureDate}.png -F 'key=linux_9ce5479100f24a04b6758d728face1c206522cd656328de92ca07b86b77482f1' https://cdn.fdmz17.eu.org/upload | grep -o 'http[s]*:[^"]*' | tr -d '\n' |xclip -sel c
+		curl -F file=@/tmp/${captureDate}.png https://0x0.st | grep -o 'http[s]*:[^"]*' | tr -d '\n' |xclip -sel c
 	elif [[ $selected = "save" ]]; then
 		ssDate=$(date '+%d-%m-%y-%H:%M:%S')
 		maim --select ~/Pictures/ss/$ssDate.png
